700a311beea1ee7d8031c290c91ddad88ea22b1f
Liliana Everett <everettliliana@gmail.com>
2019-09-11 16:05:10 +0100

    add title and display the countries list

---
+++ src/components/ListItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <li v-on:click="handleClick">{{ country.name }}</li>
+</template>
+
+<script>
+    import { eventBus } from '../main.js';
+    export default {
+        name: 'list-item',
+        props: ['country'],
+        method: {
+            handleClick(){
+                eventBus.$emit('country-selected', this.country)
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
